0d3aa43736bff0fc5554550789642e385350dc5a
Jean Park <qkrwls5998@gmail.com>
2019-08-14 20:39:07 +1200

    id added to each elements selectSize() fixed

--- cart.js
+++ cart.js
@@ -3,7 +3,7 @@ var size_name = ['S', 'M', 'L'];
 
 function addToCart() {
 	var cartHTML; //html code that replced to minicart 
-	var miniCart = document.GetElementByID("miniCart");
+	var miniCart = document.getElementById("miniCart");
 	var item_sum = 0;
 
 	for (i=0; i< sizes.length; i++) {
@@ -12,17 +12,21 @@ function addToCart() {
 			var s =sizes[i].toString();
 			var s_name = size_name[i];
 			
-
-			cartHTML += "<div class=\'Item\'>"
-				+ "<img class=\"item-img\" src=\"classic-tee.jpg\">"+
-				+"<div>"+
-				"Classic Tee" + "<br>" +
-				s + "x " + "$75.00<br>" +
-				"Size: " + s_name+
-				"</div><br>"+
-				"</div>";
+			console.log(s);
+			console.log(s_name);
+			// cartHTML += "<div class=\'Item\'>"
+			// 	+ "<img class=\"item-img\" src=\"classic-tee.jpg\">"+
+			// 	+"<div>"+
+			// 	"Classic Tee" + "<br>" +
+			// 	s + "x " + "$75.00<br>" +
+			// 	"Size: " + s_name+
+			// 	"</div><br>"+
+			// 	"</div>";
+			cartHTML += "<p>"+s + s_name+"</p>"
 		}
 	}
+	// console.log(sizes);
+	// console.log(size_name);
 	miniCart.innerHTML = cartHTML;
 	document.getElementById("sum-item").innerHTML = item_sum.toString();
 }
@@ -30,8 +34,8 @@ function addToCart() {
 function selectSize() {
 	var element = document.getElementsByName("size");
 
-	for (i = 0; i < elements.length; i++){
-		if(elements[i].checked) {
+	for (i = 0; i < element.length; i++){
+		if(element[i].checked) {
 			sizes[i] += 1;
 		}
 	}
